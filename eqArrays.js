e112dcbd5ecb423cde8dd5dc19ff41599f7ea6a5
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2021-11-23 17:03:59 -0800

    first commit

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const eqArrays = function (first,second) {
+  //if first array is longer than second.
+  for (let i = 0; i < first.length; i++) {
+    if (first[i] !== second[i]) {
+      console.log(false);
+      return;
+    } else if (i === first.length - 1 && first[i] === second[i]) {
+      console.log(true);
+      return
+    }
+  }
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
