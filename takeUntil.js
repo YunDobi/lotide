d827a1bc2affb9ff140c1c69e3e8f0a9c7f51c12
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2021-11-25 16:13:11 -0800

    takeUntil

---
+++ takeUntil.js
@@ -0,0 +1,11 @@
+const takeUntil = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    if (callback(item)) {
+      return results;
+    } else {
+      results.push(item);
+    }
+  }
+  return results;
+};
