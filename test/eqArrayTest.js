d55a5c3f6e53e2bd1d5606e74b1196915e329f81
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2021-11-29 19:33:20 -0800

    last check

--- test/eqArrayTest.js
+++ test/eqArrayTest.js
@@ -1,9 +1,9 @@
-const asssert = require("chai").assert
+const assert = require("chai").assert
 const assertEqual = require("../assertEqual")
 const eqArrays= require("../eqArrays")
 
 describe("eqArray", () => {
   it ("Assertion Paased for ([1,2] , [1,2])", () => {
-    assertEqual(eqArrays([1,2],[1,2]), true)
+    assert.equal(assertEqual(eqArrays([1,2],[1,2]), true),"✅✅✅ Assertion Passed: [true] === [true]")
   })
 })
